8637153142086ba100041507d1d9a06b81a0578e
AlanJSimpson <allanstark2908@gmail.com>
2020-10-18 13:20:15 -0300

    .

--- back-end/src/controllers/UserController.js
+++ back-end/src/controllers/UserController.js
@@ -1,18 +1,13 @@
 const registered_user = require("../models/registered_user");
-const bcrypt = require('bcrypt');
-
+const bcrypt = require("bcrypt");
 
 module.exports = {
   saveNewUser: async (req, res) => {
     let { nome, email, registerPassword, confirmPassword } = req.body;
     if (registerPassword === confirmPassword) {
+      let hashPass = await bcrypt.hash(registerPassword, 10);
       try {
-<<<<<<< HEAD
-        await UserModel.create({
-=======
-        let hashPass = await bcrypt.hash(registerPassword, 10)
         await registered_user.create({
->>>>>>> 44045dbb9f325c825f1ab5aa76701eaf06859a25
           nome,
           email,
           senha: hashPass,
