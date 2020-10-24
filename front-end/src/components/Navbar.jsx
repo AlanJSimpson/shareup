e607643e42f5a5eacf37dc8abb28a9ac494fb2cf
RenanFelice <felice_owns@hotmail.com>
2020-10-24 12:11:29 -0300

    adicionado menu no botão do navbar

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -4,13 +4,17 @@ import cx from 'classnames'
 import HamburguerToX from './HamburguerToX'
 
 function Navbar() {
-    const [showMenu, setShowMenu] = useState(false)
-    const [hamburguerIsClicked, setHamburguerIsClicked] = useState(false)
+    const [showMenu, setShowMenu] = useState(false);
+    const [hamburguerIsClicked, setHamburguerIsClicked] = useState(false);
+    const [configMenuClicked, setConfigMenuClicked] = useState(false);
 
     window.addEventListener('resize', e => {
         if (window.innerWidth >= 960) {
             setShowMenu(false);
             setHamburguerIsClicked(false)
+        }
+        if (window.innerWidth <= 960) {
+            setConfigMenuClicked(false)
         }
     })
 
@@ -25,8 +29,19 @@ function Navbar() {
                     <li className={styles.navItem}>Principal</li>
                     <li className={styles.navItem}>Meus Eventos</li>
                     <li className={styles.navItem}>Meu Perfil</li>
-                    {/* <button className={styles.navbarBtn}>Sair</button> */}
-                    <div className={styles.navbarBtn}><span className={styles.navbarBtnText}>S</span></div>
+                    <div className={styles.navbarBtn}
+                        onClick={e => setConfigMenuClicked(!configMenuClicked)}
+                    >
+                        <span className={styles.navbarBtnText}>S</span>
+                    </div>
+                    {configMenuClicked &&
+                        <div className={styles.configMenu}>
+                            <ul className={styles.configMenuList}>
+                                <li className={styles.configMenuItem}>item 1</li>
+                                <li className={styles.configMenuItem}>item 2</li>
+                                <li className={styles.configMenuItem}>item 3</li>
+                            </ul>
+                        </div>}
                 </ul>
                 <HamburguerToX hamLineHeight={5} hamColor={'white'} hamContainerSize={30} showMenu={showMenu} setShowMenu={setShowMenu} hamburguerIsClicked={hamburguerIsClicked} setHamburguerIsClicked={setHamburguerIsClicked} />
 
@@ -38,7 +53,7 @@ function Navbar() {
                         <div className={styles.btnContainer}>
                             <button className={styles.sideMenuBtn}>Sign Up</button>
                         </div>
-                        
+
                     </ul>
                 </div>
             </div>
