01f2b2792224fe1b4252fe7bd1d3539dfb5421b8
RenanFelice <felice_owns@hotmail.com>
2020-10-24 18:36:27 -0300

    adicionado categorias no menu lateral

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -7,6 +7,7 @@ function Navbar() {
   const [showMenu, setShowMenu] = useState(false);
   const [hamburguerIsClicked, setHamburguerIsClicked] = useState(false);
   const [configMenuClicked, setConfigMenuClicked] = useState(false);
+  const [categoriesMenuOpen, setCategoriesMenuOpen] = useState(false);
 
   window.addEventListener("resize", () => {
     if (window.innerWidth >= 960) {
@@ -56,12 +57,24 @@ function Navbar() {
         />
 
         <div className={cx(styles.sideMenu, { [styles.menuActive]: showMenu })}>
-          <ul>
+          <ul className={styles.sindeMenuList}>
             <li className={styles.sideMenuItem}>Principal</li>
             <li className={styles.sideMenuItem}>Meus Eventos</li>
             <li className={styles.sideMenuItem}>Meu Perfil</li>
-            <li className={styles.sideMenuItem}>Categorias</li>
-            <div className={styles.btnContainer}>
+            <li className={styles.sideMenuItem}
+            onClick={e => setCategoriesMenuOpen(!categoriesMenuOpen)}
+            >Categorias <i className={cx("fas fa-chevron-right", styles.categoriesIcon, { [styles.categoriesIconActive]: categoriesMenuOpen })}></i></li>
+            <div className={styles.categoriesMenu}>
+                <ul className={styles.categoriesMenuList}>
+                    <li className={styles.categoriesMenuItem}>Esportes</li>
+                    <li className={styles.categoriesMenuItem}>Idioma</li>
+                    <li className={styles.categoriesMenuItem}>Música</li>
+                    <li className={styles.categoriesMenuItem}>Auto ajuda</li>
+                    <li className={styles.categoriesMenuItem}>Tecnologia</li>
+                    <li className={styles.categoriesMenuItem}>Arte</li>
+                </ul>
+            </div>
+            <div className={cx(styles.btnContainer, { [styles.btnContainerActive]: categoriesMenuOpen })}>
               <button className={styles.sideMenuBtn}>Sair</button>
             </div>
           </ul>
